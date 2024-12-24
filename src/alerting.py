613a1108cef53dcef9eb9c8da7ff0650e0ee567b
Pawar-Tushar <157030970+Pawar-Tushar@users.noreply.github.com>
2024-12-24 20:10:48 +0530

    First file commit to git
    all initial file uploded

---
+++ src/alerting.py
@@ -0,0 +1,13 @@
+# # import smtplib
+# # from email.mime.text import MIMEText
+
+# # def send_email_alert(subject, message):
+# #     msg = MIMEText(message)
+# #     msg["Subject"] = subject
+# #     msg["From"] = "your_email@example.com"
+# #     msg["To"] = "admin_email@example.com"
+# #     server = smtplib.SMTP("smtp.example.com")
+# #     server.sendmail("sender", "reciver", msg.as_string())
+# #     server.quit()
+
+
